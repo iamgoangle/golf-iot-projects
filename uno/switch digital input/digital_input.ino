ddb559bc61faf14022496bb2b739771ff7b719ab
Teerapong Singthong <st.teerapong@gmail.com>
2019-05-12 00:30:12 +0700

    fix: change to input pull up

--- uno/switch digital input/digital_input.ino
+++ uno/switch digital input/digital_input.ino
@@ -4,12 +4,12 @@ int sw = 8;
 void setup()
 {
   Serial.begin(9600); // watch serial monitor
-  pinMode(sw, INPUT);
+  pinMode(sw, INPUT_PULLUP);
 }
 
 void loop()
 {
   int val = digitalRead(sw);
   Serial.println(val);
-  delay(1000);
+  delay(600);
 }
