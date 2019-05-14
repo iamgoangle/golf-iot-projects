50f3a0cbd8aef240f3e5c4c515aacf6ce9307adc
Teerapong Singthong <st.teerapong@gmail.com>
2019-05-15 01:12:36 +0700

    feat: readme for ldr project

--- uno/ldr/README.md
+++ uno/ldr/README.md
@@ -6,3 +6,7 @@
 
 - Read input from `LDR` sensor and show it on Serial monitor.
 - Get LDR sensor when room is dark and turn on the LED. (eliminate darkness)
+
+## Assemble
+
+![Assemble](https://raw.githubusercontent.com/iamgoangle/golf-iot-projects/master/uno/ldr/ldr_led.jpg)
--- uno/ldr/lrd/lrd.ino
+++ uno/ldr/lrd/lrd.ino
@@ -1,15 +1,26 @@
 int ldr = A0;
+int led = 11;
 
 void setup()
 {
   Serial.begin(9600);
+  
   pinMode(ldr, INPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop()
 {
   int ldrVal = analogRead(ldr);
+
+  Serial.println("LDR Sensor");
   Serial.println(ldrVal);
+
+  if (ldrVal > 400) {
+    analogWrite(led, 255);
+  } else {
+    analogWrite(led, 0);
+  }
 
   delay(500);
 }
