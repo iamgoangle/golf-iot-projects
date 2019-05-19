e7c722ec36fc5ece6f43df89b06f20ea61fa5c66
Teerapong Singthong <st.teerapong@gmail.com>
2019-05-20 01:21:17 +0700

    feat: lcd project :

---
+++ uno/lcd/README.md
@@ -0,0 +1,7 @@
+# LCD 16x2 + I2C Module
+
+![LCD](https://raw.githubusercontent.com/iamgoangle/golf-iot-projects/master/uno/lcd/demo.GIF)
+
+## Objective
+
+- Display text on LCD.
---
+++ uno/lcd/lcd/lcd.ino
@@ -0,0 +1,77 @@
+#include <Wire.h>
+#include <LiquidCrystal_PCF8574.h>
+
+// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_PCF8574 lcd(0x27);
+
+void clearScreen() {
+  lcd.home();
+  lcd.clear();
+}
+
+void checkLCD() {
+  int error;
+
+  while (! Serial);
+
+  Serial.println("Dose: check for LCD");
+
+  // See http://playground.arduino.cc/Main/I2cScanner
+  Wire.begin();
+  Wire.beginTransmission(0x27);
+  error = Wire.endTransmission();
+  Serial.print("Error: ");
+  Serial.print(error);
+
+  if (error == 0) {
+    Serial.println(": LCD found.");
+
+  } else {
+    Serial.println(": LCD not found.");
+  } // if
+}
+
+void setup() {
+  Serial.begin(9600);
+  checkLCD();
+
+  lcd.begin(16, 2);  // initialize 16x2 pixel
+}
+
+void printHello() {
+  clearScreen();
+
+  lcd.setCursor(0, 0);
+  lcd.setBacklight(255);
+  lcd.display();
+  lcd.blink();
+
+  String name = "Hello! Golf";
+  lcd.print(name);
+  delay(3000);
+}
+
+void printMedium() {
+  clearScreen();
+
+  lcd.setBacklight(255);
+  lcd.display();
+  lcd.noBlink();
+
+  // 1st line
+  lcd.setCursor(0, 0);
+  String blog = "medium.com";
+  lcd.print(blog);
+
+  // 2nd line
+  lcd.setCursor(0, 1);
+  lcd.cursor();
+  String user = "/iamgoangle";
+  lcd.print(user);
+  delay(3000);
+}
+
+void loop() {
+  printHello();
+  printMedium();
+}
